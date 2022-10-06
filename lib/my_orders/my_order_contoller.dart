c4ec8ae83ec2181da842a21233dc43f055016b29
unknown <prathamnatekar@yahoo.in>
2022-10-06 22:14:29 +0530

    ecommerce

---
+++ lib/my_orders/my_order_contoller.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: avoid_print
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce/my_orders/model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class MyOrderController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  List<MyOrdersModel> model = [];
+  bool isLoading = true;
+
+  Future<void> getMyOrders() async {
+    try {
+      await _firestore
+          .collection('users')
+          .doc(_auth.currentUser!.uid)
+          .collection('myorders')
+          .get()
+          .then((value) {
+        model =
+            value.docs.map((e) => MyOrdersModel.fromJson(e.data())).toList();
+
+        isLoading = false;
+        update();
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void onInit() {
+    // ignore: todo
+    // TODO: implement onInit
+    super.onInit();
+    getMyOrders();
+  }
+}
